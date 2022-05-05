7bb01f34dda3bace31849aeb130eeba3cc878ce9
n-t-roff <troff@arcor.de>
2022-05-05 17:10:05 +0200

    version updated

--- ver.h
+++ ver.h
@@ -21,5 +21,5 @@
  *            failes, this will become the release version. */
 
 #ifndef VERSION
-#define VERSION "1.14.0-pre-alpha 2022-04-06"
+#define VERSION "1.14.0-pre-alpha 2022-05-04"
 #endif
